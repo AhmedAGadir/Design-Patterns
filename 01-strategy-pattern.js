596c9714e10de0b266fc422cfcb057d9b614ea78
Ahmed Abdel Gadir <ahmed.gadir@gmail.com>
2018-12-27 00:06:52 +0000

    update

--- 01-strategy-pattern.js
+++ 01-strategy-pattern.js
@@ -57,52 +57,52 @@ new Client(new StrategyC).doSomething();
 
 // different syntax
 
-var StrategyA = function () {
-    this.doSomething = function () {
-        // does something
-    }
-}
-var StrategyB = function () {
-    this.doSomething = function () {
-        // does something
-    }
-}
-var StrategyC = function () {
-    this.doSomething = function () {
-        // does something
-    }
-}
+// var StrategyA = function () {
+//     this.doSomething = function () {
+//         // does something
+//     }
+// }
+// var StrategyB = function () {
+//     this.doSomething = function () {
+//         // does something
+//     }
+// }
+// var StrategyC = function () {
+//     this.doSomething = function () {
+//         // does something
+//     }
+// }
 
-var Client = function () {
-    this.strategy = null;
-}
-Client.prototype.setStrategy = function (strat) {
-    this.strategy = strat
-}
-Client.prototype.doSomething = function () {
-    this.strategy.doSomething();
-}
+// var Client = function () {
+//     this.strategy = null;
+// }
+// Client.prototype.setStrategy = function (strat) {
+//     this.strategy = strat
+// }
+// Client.prototype.doSomething = function () {
+//     this.strategy.doSomething();
+// }
 
 
 // // could do:
 
-var methodOne = new StrategyA();
-var methodTwo = new StrategyB();
-var methodThree = new StrategyC();
+// var methodOne = new StrategyA();
+// var methodTwo = new StrategyB();
+// var methodThree = new StrategyC();
 
-var myClient = new Client();
-myClient.setStrategy(methodOne);
-myClient.doSomething();
-myClient.setStrategy(methodTwo);
-myClient.doSomething();
-myClient.setStrategy(methodThree);
-myClient.doSomething();
+// var myClient = new Client();
+// myClient.setStrategy(methodOne);
+// myClient.doSomething();
+// myClient.setStrategy(methodTwo);
+// myClient.doSomething();
+// myClient.setStrategy(methodThree);
+// myClient.doSomething();
 
 // // or 
 
-newClient().setStrategy(methodOne).doSomething();
-newClient().setStrategy(methodTwo).doSomething();
-newClient().setStrategy(methodThree).doSomething();
+// newClient().setStrategy(methodOne).doSomething();
+// newClient().setStrategy(methodTwo).doSomething();
+// newClient().setStrategy(methodThree).doSomething();
 
 
 
